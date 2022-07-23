3c676f5e0361dff1998bca55f3afb43dbafe3aaa
Rui Oliveira <rpoliveira02@gmail.com>
2022-07-23 20:39:57 +0100

    Add request parsing + datatypes

---
+++ src/main/java/ConditionalExchange.java
@@ -0,0 +1,73 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A conditional exchange. A conditional exchange contains a set of "simple exchanges" that should either all be performed
+ * or else none can be performed. For example, "Shift from TP1 -> TP2 in course A if and only if shift from TP3 -> TP4 in
+ * course B
+ */
+public class ConditionalExchange {
+    /**
+     * The unique identifier of the conditional exchange
+     */
+    private int id;
+
+    /**
+     * The timestamp of the exchange, i.e., when it was requested
+     */
+    private long timestamp;
+
+    /**
+     * The student who requested the exchange
+     */
+    private String student;
+
+    /**
+     * The "simple" exchanges that make up the conditional exchange
+     */
+    private List<Exchange> exchanges;
+
+    /**
+     * Parameterized constructor
+     * @param id the identifier of the exchange
+     * @param timestamp the timestamp of the exchange
+     * @param student the student who requested the exchange
+     * @param exchanges the "simple" exchanges that make up the conditional exchange
+     */
+    public ConditionalExchange(int id, long timestamp, String student, List<Exchange> exchanges) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.student = student;
+        this.exchanges = exchanges;
+    }
+
+    /**
+     * Constructor from a JSON object
+     * @param object the JSON object
+     * @throws JSONException if the JSON object is not of the correct format
+     */
+    public ConditionalExchange(JSONObject object) throws JSONException {
+        this(object.getInt("id"), object.getLong("timestamp"), object.getString("student"),
+                ConditionalExchange.listFromJSON(object.getJSONArray("exchanges")));
+    }
+
+    /**
+     * Converts the JSON Array of the "simple" exchanges to a list of exchanges
+     * @param array the JSON array
+     * @return the list of exchanges
+     * @throws JSONException if the provided JSON is not valid
+     */
+    private static List<Exchange> listFromJSON(JSONArray array) throws JSONException {
+        List<Exchange> exchanges = new ArrayList<>();
+
+        for(int i = 0; i < array.length(); i++) {
+            exchanges.add(new Exchange(array.getJSONObject(i)));
+        }
+
+        return exchanges;
+    }
+}
--- src/main/java/SwapSolver.java
+++ src/main/java/SwapSolver.java
@@ -1,4 +1,5 @@
 import static java.util.stream.Collectors.toList;
+import static spark.Spark.initExceptionHandler;
 import static spark.Spark.threadPool;
 
 import java.io.FileNotFoundException;
@@ -11,16 +12,79 @@ import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import com.google.ortools.Loader;
 
+/**
+ * Main application class
+ */
 public class SwapSolver {
 
 	public static void main(String[] args) throws JSONException {
 
-		com.google.ortools.Loader.loadNativeLibraries();
+		Loader.loadNativeLibraries();
 
 		threadPool(8);
-		Spark.post("/", (req, res) -> LPSolver.solve(req.body()));
+		Spark.post("/", (req, res) -> processRequest(req, res));
+	}
+
+	public static String processRequest(Request request, Response response) {
+		try {
+			List<ConditionalExchange> exchangeRequests = parseRequest(request.body());
+			if(!validateRequest(exchangeRequests)) {
+				response.status(400);
+				return errorToJSON("Set of exchanges is not valid");
+			}
+		} catch(JSONException) {
+			response.status(400);
+			return errorToJSON("Body not a valid JSON object");
+		}
+
+		response.status(200);
+		return "Batata";
+	}
+
+	//TODO
+
+	/**
+	 * Determines whether or not a list of exchanges corresponds to a valid request
+	 *
+	 * TODO: DEFINE VALID REQUEST
+	 *
+	 * @param requestedExchanges the list of requested exchanges
+	 * @return whether or not the requested exchanges make up a valid request
+	 */
+	private static boolean validateRequest(List<ConditionalExchange> requestedExchanges) {
+		return true;
+	}
+
+	/**
+	 * Converts an error message to a JSON string
+	 * @param error the error message
+	 * @return the JSON string
+	 */
+	private static String errorToJSON(String error) {
+		return String.format("{\"error\": \"%s\"}", error);
+	}
+
+	/**
+	 * Parses the request's body to the list of requested exchanges
+	 * @param request the JSON string of the request
+	 * @return the list of requested exchanges
+	 * @throws JSONException if the string is not valid JSON or it is not of the correct format
+	 */
+	private static List<ConditionalExchange> parseRequest(String request) throws JSONException {
+		List<ConditionalExchange> requestedExchanges = new ArrayList<>();
+		JSONObject object = new JSONObject(request);
+		JSONArray array = object.getJSONArray("requested_exchanges");
+
+		for(int i = 0; i < array.length(); i++) {
+			requestedExchanges.add(new ConditionalExchange(array.getJSONObject(i)));
+		}
+
+		return requestedExchanges;
 	}
 
 	/**
@@ -31,7 +95,7 @@ public class SwapSolver {
 	 * @return Returns a List of ExchangeRequest objects containing the same
 	 *         information as the given parameter
 	 */
-	public static List<ExchangeRequest> parseRequestsFromJSON(JSONArray courseExchanges) {
+	/*public static List<ExchangeRequest> parseRequestsFromJSON(JSONArray courseExchanges) {
 		ArrayList<ExchangeRequest> requests = new ArrayList<>();
 		for (int j = 0; j < courseExchanges.length(); j++) {
 			// iterate over every exchange request of a given course
@@ -44,5 +108,5 @@ public class SwapSolver {
 			requests.add(er);
 		}
 		return requests;
-	}
+	}*/
 }
